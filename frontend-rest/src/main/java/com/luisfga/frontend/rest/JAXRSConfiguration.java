f2dfbde24b3dd45d76d0af3375a8a9034fa59ac7
luisfga <luisfga@gmail.com>
2020-10-28 02:48:12 -0300

    Criação do módulos rest

---
+++ frontend-rest/src/main/java/com/luisfga/frontend/rest/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.luisfga.frontend.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("rest")
+public class JAXRSConfiguration extends Application {
+    
+}
